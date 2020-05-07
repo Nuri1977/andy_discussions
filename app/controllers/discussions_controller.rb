55e33944e822290884a470d0e3f5d9d58ea13ac8
nuri1977 <nurilacka@gmail.com>
2020-05-07 17:30:41 +0200

    Discussions new view eddited

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,6 +1,6 @@
 class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:show, :edit, :update, :destroy]
-  before_action :find_channels, only: [:index, :show, :new, :edit]
+  before_action :find_channels, only: [:index, :show, :new, :edit, :create]
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /discussions
